b764212789341e9892897bb3f2f9ab9a0a1b82a3
Anthony Barba <abarba222@gmail.com>
2015-05-15 16:35:48 -0400

    JavaScript

---
+++ main.js
@@ -0,0 +1,38 @@
+var varButton = [];
+var Equals = 0;
+
+function alertButtonValue(event){
+  var button = event.target;
+  console.log(button);
+  var text = button.textContent;
+  varButton.push(text);
+  console.log(varButton);
+  alert(text);
+}
+
+[].forEach.call(document.querySelectorAll('.keypad-button'), function(element){
+  element.addEventListener('click', alertButtonValue);
+}, false);
+
+
+function alertEquals(event){
+  var str = varButton.join("");
+  console.log(str);
+  Equals = eval(str);
+  console.log(Equals);
+}
+
+[].forEach.call(document.querySelectorAll('.equals'), function(element){
+  element.addEventListener('click', alertEquals);
+}, false);
+
+function alertClear(event){
+  Equals = 0;
+  varButton = [];
+  console.log(varButton);
+  alert(Equals);
+}
+
+[].forEach.call(document.querySelectorAll('.ac'), function(element){
+  element.addEventListener('click', alertClear);
+}, false);
